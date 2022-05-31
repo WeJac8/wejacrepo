91bc1147bb36730c9bbba9af540bb13e7746462b
nicemanwejac <nicemanwejac@163.com>
2022-06-01 00:02:41 +0800

    增加test的副本

---
+++ test - 副本.py
@@ -0,0 +1,15 @@
+#this is a test file for git push test
+class MyInt:
+	__value = None
+	def __init__(self,value): 
+		self.value = value
+	def __len__(self):
+		return len( str(self.value) )
+	@staticmethod
+	def test_() -> (int,str):
+		pass
+class MyInt2(MyInt):
+	pass
+import  sys
+if __name__ == '__main__':
+	print(isinstance(MyInt2(10),MyInt))
